2196ffcd96a832866342a20f447c984a77492a6f
MasoudFarmani <info.masoudfarmani@gmail.com>
2023-10-03 02:26:14 +0330

    Add IBOutlet references to UI Views

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -2,5 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var zeroPctButton: UIButton!
 }
 
