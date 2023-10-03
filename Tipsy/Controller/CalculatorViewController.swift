7f284299553581e9332dc0e00a6cf0a7caf5281d
MasoudFarmani <info.masoudfarmani@gmail.com>
2023-10-03 19:42:31 +0330

    Implement the functionality of `tipChanged` method

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -6,14 +6,26 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var zeroPctButton: UIButton!
+
+    var calculatorBrain = CalculatorBrain()
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        selectTheButtonDeselectOthers(sender)
+        let buttonTitle = sender.currentTitle
+        calculatorBrain.setTipPercentage(buttonTitle)
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    func selectTheButtonDeselectOthers(_ button: UIButton){
+        twentyPctButton.isSelected = false
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        button.isSelected = true
     }
 }
 
