680795d6cc1b87bfcebb408b1b723d1ced8e4576
MasoudFarmani <info.masoudfarmani@gmail.com>
2023-10-03 19:05:13 +0330

    Create `CalculatorBrain` struct

---
+++ Tipsy/Controller/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  Tipsy
+//
+//  Created by Masoud Farmani on 11.07.1402 AP.
+//  Copyright © 1402 AP The App Brewery. All rights reserved.
+//
+
+struct CalculatorBrain {
+    var billTotal: Double
+    var tipPercentage: Double
+    var splitCount: Int
+}
